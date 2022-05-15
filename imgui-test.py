a2be638d1cfa919c4b74767c640033b5bd9bb015
Ben <benwithington@hotmail.com>
2022-05-15 13:01:38 +0100

    ImGui Base

--- imgui-test.py
+++ imgui-test.py
@@ -1,9 +1,11 @@
 import glfw
 from OpenGL.GL import *
+import imgui
+from imgui.integrations.glfw import GlfwRenderer
 
-width  = 640
-height = 480
-title  = "PyImGUI Test"
+width  = 1280
+height = 720
+title  = "pyimgui test"
 
 def main():
     if not glfw.init():
@@ -19,19 +21,40 @@ def main():
     glfw.make_context_current(window)
     glfw.set_framebuffer_size_callback(window, framebufferSizeCallback)
 
+    #imgui setup
+    imgui.create_context()
+    io = imgui.get_io()
+    impl = GlfwRenderer(window)
+
     while not glfw.window_should_close(window):
 
         #User Inputs
         processInput(window)
+        impl.process_inputs()
+
+        #imgui start frame
+        imgui.new_frame()
 
         #Render Stuff
         glClearColor(0.1, 0.1, 0.1, 1.0)
         glClear(GL_COLOR_BUFFER_BIT)
 
+        #Create imgui window
+        #imgui.show_demo_window()
+        gui(window)
+
+        #Render imgui stuff
+        imgui.render()
+        impl.render(imgui.get_draw_data())
+        
+        #End imgui frame
+        imgui.end_frame()
+
         #Swap Buffers & poll events
         glfw.swap_buffers(window)
         glfw.poll_events()
 
+    impl.shutdown()
     glfw.terminate()
 
 def processInput(window: glfw._GLFWwindow):
@@ -42,6 +65,20 @@ def processInput(window: glfw._GLFWwindow):
 
 def framebufferSizeCallback(glfw_window: glfw._GLFWwindow, width: int, height: int):
     glViewport(0, 0, width, height)
+
+def gui(glfw_window: glfw._GLFWwindow):
+    if imgui.begin_main_menu_bar():
+        if imgui.begin_menu("File"):
+            clicked, selected = imgui.menu_item("Quit", "Ctrl+Q")
+            if clicked:
+                glfw.set_window_should_close(glfw_window, True)
+            imgui.end_menu()
+        imgui.end_main_menu_bar()
+
+    imgui.begin("A Window")
+    imgui.text("Hello from a window")
+    imgui.end()
+                
 
 if __name__ == "__main__":
     main()
